0660bb9ff17b6114a7a60326f493289d9e635b6b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-10-15 12:14:56 +0530

    Update main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./components/Home"
-import SingleMovie from "./components/SingleMovie"
-import Error from "./components/Error"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SingleMovie from "./components/SingleMovie";
+import Error from "./components/Error";
 
 function App() {
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="movie/:id" element={<SingleMovie /> } />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movie/:id" element={<SingleMovie />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
